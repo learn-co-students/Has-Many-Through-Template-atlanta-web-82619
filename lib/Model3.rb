3f79e2dcaf31c85fb616b6b5f3674041d6c13f0e
nicklunn325 <nrlunn325@gmail.com>
2019-09-01 13:29:59 -0400

    Done.

--- lib/Model3.rb
+++ lib/Model3.rb
@@ -1,5 +1,20 @@
-#class for Model3 goes here
-#Feel free to change the name of the class
-class Model3
-  
+class StudySession
+    attr_reader :subject , :student , :teacher
+    attr_accessor :status
+    @@all = []
+    def initialize(subject, student, teacher)
+        @subject = subject
+        @student = student
+        @teacher = teacher
+        @status = 'scheduled'
+        @@all << self
+    end
+    def execute_study
+        if self.status == 'scheduled'
+            @status = 'completed'
+        end
+    end
+    def self.all
+        @@all
+    end
 end
